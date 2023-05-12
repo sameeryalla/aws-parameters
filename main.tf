92f02b37942570ec5458e04e870bead21f51bc50
sameeryalla <sameeryalla@gmail.com>
2023-05-12 18:58:19 +0530

    updated key patrameters 2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
   count=length(var.parameters)
   name = var.parameters[count.index].name
-  type = "SecureString"
+  type = "String"
   key_id = "ee630d04-63d5-43bf-b139-108ebd656d7d"
   value = var.parameters[count.index].value
 }
