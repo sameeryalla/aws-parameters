177e917415966b5373874625148455039cfe157c
sameeryalla <sameeryalla@gmail.com>
2023-05-12 18:43:33 +0530

    added repo for parameters with KMS id

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name = "test.test1"
-  type = "String"
+  type = "SecureString"
+  key_id = "ee630d04-63d5-43bf-b139-108ebd656d7d"
   value = "Hello World"
 }
