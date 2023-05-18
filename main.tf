d7746105602e3b28ce3a328e9d7ee6faea21cd0a
sameeryalla <sameeryalla@gmail.com>
2023-05-18 20:15:16 +0530

    parameter store key id changed 2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameter" {
   name = var.parameters[count.index].name
   value = var.parameters[count.index].value
   type = "String"
-  key_id = "ee630d04-63d5-43bf-b139-108ebd656d7d"
+  key_id = "10c42bec-f042-4d6d-8509-7e3e918c6e0e"
 }
 
 resource "aws_ssm_parameter" "passwords" {
