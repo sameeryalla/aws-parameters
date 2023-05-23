fce37f488c32599f34fb0672328556e5598888de
sameeryalla <sameeryalla@gmail.com>
2023-05-23 22:12:15 +0530

    raghu code deployed 8

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameters" {
   name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
   type   = "String"
-  key_id = "dce90622-5a23-4f82-a639-be841f534702"
+  key_id = "10c42bec-f042-4d6d-8509-7e3e918c6e0e"
 }
 
 ## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
@@ -13,5 +13,5 @@ resource "aws_ssm_parameter" "passwords" {
   name   = var.passwords[count.index].name
   value  = var.passwords[count.index].value
   type   = "SecureString"
-  key_id = "dce90622-5a23-4f82-a639-be841f534702"
+  key_id = "10c42bec-f042-4d6d-8509-7e3e918c6e0e"
 }
